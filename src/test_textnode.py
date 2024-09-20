52e52c2db921295fc5df2ce0ec4685ea6b57bb30
AlexanderArrr <alexanderaretzger@gmail.com>
2024-09-20 14:45:00 +0200

    TextNode test cases added

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text MODE", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_type_not_eq(self):
+        node = TextNode("This is a text node", "italic")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode("This is a text node", "bold", None)
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_type_url_not_eq(self):
+        node = TextNode("This is a text node", "bold", "http://test.com")
+        node2 = TextNode("This is a text node", "normal", "http://test.com")
+        self.assertNotEqual(node, node2)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
