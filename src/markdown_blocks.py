1ee99b006ce23d6d99aa60f25a1c0921adbc0d32
AlexanderArrr <alexanderaretzger@gmail.com>
2024-09-24 17:15:02 +0200

    Block -> Blocktype added

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,11 @@
+import re
+
+block_type_code = "code"
+block_type_heading = "heading"
+block_type_ordered_list = "ordered_list"
+block_type_paragraph = "paragraph"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
 
 def markdown_to_blocks(markdown):
     lines_list = markdown.split('\n\n')
@@ -8,3 +16,30 @@ def markdown_to_blocks(markdown):
         line = line.strip()
         filtered_lines.append(line)
     return filtered_lines
+
+def block_to_block_type(block):
+    if block.startswith('```') and block.endswith('```'):
+        return block_type_code
+    if block.startswith(('# ', '## ', '### ', '#### ', '##### ', '###### ')):
+        return block_type_heading
+    if block.startswith('>'):
+        split_block = block.split('\n')
+        for line in split_block:
+            if not line.startswith('>'):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith('* ') or block.startswith('- '):
+        split_block = block.split('\n')
+        for line in split_block:
+            if not line.startswith('* ') and not line.startswith('- '):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if block.startswith('1. '):
+        split_block = block.split('\n')
+        list_number = 1
+        for line in split_block:
+            if not line.startswith(f"{list_number}. "):
+                return block_type_paragraph
+            list_number += 1
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,8 +1,19 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (markdown_to_blocks,
+                             block_to_block_type,
+                             block_type_code,
+                             block_type_heading,
+                             block_type_ordered_list,
+                             block_type_paragraph,
+                             block_type_quote,
+                             block_type_unordered_list
+                            )
 
 
 class TestMarkdownToHTML(unittest.TestCase):
+
+    # TESTS FOR markdown_to_block
+
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
@@ -45,6 +56,78 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+    
+    # -------------------------------------------------
+    # TESTS FOR block_to_block_type
+
+    def test_headings_block_type(self):
+        md = "# This is a heading"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_heading)
+    
+    def test_3_headings_block_type(self):
+        md = "### This is a heading"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_heading)
+
+    def test_6_headings_block_type(self):
+        md = "###### This is a heading"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_heading)
+    
+    def test_7_headings_block_type(self):
+        md = "####### This is not a heading"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_code_block_type(self):
+        md = "```This is\na\ncode block```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_code)
+
+    def test_not_code_block_type(self):
+        md = "```This is not\na code block``"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_paragraph)
+    
+    def test_quote_block_type(self):
+        md = "> This is a\n> quote block\n> with 3 lines"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_quote)
+
+    def test_not_quote_block_type(self):
+        md = ">This is not\n> a correct\nquote block"
+        self.assertRaises(ValueError, block_to_block_type, md)
+
+    def test_unordered_list_block_type(self):
+        md = md = "* This is\n* an\n* unordered list\n* with 4 entries"
+        md2 = "* This is\n* an unordered list"
+        md3 = "- This is\n- an unordered\n- list"
+        self.assertEqual(block_to_block_type(md), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(md2), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(md3), block_type_unordered_list)
+
+    def test_not_unordered_list_block_type(self):
+        md = "* This is\nnot\n* an unordered list\n* with 3 entries"
+        md2 = "* This is\n* not\nan unordered list"
+        with self.assertRaises(ValueError):
+            block_to_block_type(md)
+            block_to_block_type(md2)
+
+    def test_ordered_list_block_type(self):
+        md = "1. This is\n2. an ordered\n3. list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+    def test_not_ordered_list_block_type(self):
+        md = "1. This is\n2. not\nan ordered list"
+        md2 = "1. This is\nn2.not\n3. an ordered list"
+        with self.assertRaises(ValueError):
+            block_to_block_type(md)
+            block_to_block_type(md2)
+
+    
+
 
 
 if __name__ == "__main__":
