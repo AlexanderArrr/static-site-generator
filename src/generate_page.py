fd18fd9e21b1d0fb722755d7548d1299d3819484
AlexanderArrr <alexanderaretzger@gmail.com>
2024-09-26 15:19:36 +0200

    Recursive generation added, project complete

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,6 +2,7 @@ import os
 
 from markdown_html import markdown_to_html_node
 from htmlnode import ParentNode
+from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
@@ -19,6 +20,36 @@ def generate_page(from_path, template_path, dest_path):
     dest_file = open(dest_path, "a")
     dest_file.write(template_string)
     dest_file.close()
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    new_content_path = Path(dir_path_content)
+    new_template_path = Path(template_path)
+    new_dest_path = Path(dest_dir_path)
+
+    if not os.path.exists(new_dest_path):
+        os.mkdir(new_dest_path)
+
+    content_file_list = os.listdir(new_content_path)
+    for entry in content_file_list:
+        entry_content_path = os.path.join(new_content_path, entry)
+        entry_dest_path = os.path.join(new_dest_path, entry)
+        if not os.path.isfile(entry_content_path):
+            generate_pages_recursive(entry_content_path, template_path, entry_dest_path)
+        if not os.path.exists(entry_dest_path):
+            md_string = open(entry_content_path).read()
+            template_string = open(new_template_path).read()
+
+            html_node = markdown_to_html_node(md_string)
+            html_string = html_node.to_html()
+
+            template_string = template_string.replace('{{ Title }}', extract_title(md_string))
+            template_string = template_string.replace('{{ Content }}', html_string)
+
+            entry_dest_path = entry_dest_path.replace('.md', '.html')
+            dest_file = open(entry_dest_path, "a")
+            dest_file.write(template_string)
+            dest_file.close()
 
 
 def extract_title(markdown):
--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import os, shutil
 
 from copy_static import copy_content_src_to_dest
-from generate_page import generate_page
+from generate_page import (generate_page,
+                           generate_pages_recursive)
 
 path_public = './public'
 path_static = './static'
 
-path_from = './content/index.md'
+path_from = './content/'
 path_template = './template.html'
-path_dest = './public/index.html'
+path_dest = './public/'
 
 def main():
     print("Deleting public directory..")
@@ -19,7 +20,7 @@ def main():
     copy_content_src_to_dest(path_static, path_public)
     print("Creation of folder(s) and copying of files successful!")
 
-    generate_page(path_from, path_template, path_dest)
+    generate_pages_recursive(path_from, path_template, path_dest)
 
 
 main()
